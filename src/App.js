677fcd710b41500a67edbf03bf45b886797050d2
Kavan Samra <kavansamra@gmail.com>
2020-03-01 14:39:39 -0800

    added new web-font and setting global styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-import Card from './components/Card/Card';
 
 function App() {
   return (
-    <Card />
+    <div>Hello World</div>
   );
 }
 
---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import {createGlobalStyle, ThemeProvider} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-size: 62.5%;
+    box-sizing: border-box;
+    
+    
+    
+  }
+`;
+
+
+export default GlobalStyle;
