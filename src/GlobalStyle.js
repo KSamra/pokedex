878ef513b3fa37176bf58c02d6275e44dc4df07e
Kavan Samra <kavansamra@gmail.com>
2020-03-01 16:20:42 -0800

    set global styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,7 +4,10 @@ const GlobalStyle = createGlobalStyle`
   body{
     font-size: 62.5%;
     box-sizing: border-box;
-    
+    font-family: 'Open-Sans', sans-serif;
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     
     
   }
