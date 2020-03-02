111091546ba25112aef5b1865f522bcbff017362
Kavan Samra <kavansamra@gmail.com>
2020-03-01 22:56:51 -0800

    minor change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+//import GlobalStyle from './GlobalStyle';
+import styled from 'styled-components';
+
+
 
 
 function App() {
   return (
-    <div>Hello World</div>
-  );
+
+  <div>Hello</div>  
+  )
 }
 
 export default App;
