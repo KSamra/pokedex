c7a73e33a1bbf9adc4f9d559fc03932a1c371129
Kavan Samra <kssamra@ucsc.edu>
2020-03-06 18:17:48 -0800

    added pokemon type icons, these will be moved to an image store in the future

--- src/components/AppContainer/Container.js
+++ src/components/AppContainer/Container.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import Header from '../Header/Header';
 import Filter from '../Filter/Filter';
