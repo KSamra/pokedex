17efc336bd5d25a34076eea16eb7ec576db20602
Kavan Samra <kssamra@ucsc.edu>
2020-03-09 11:55:38 -0700

    change to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 const Header = styled.header`
   grid-area: header;
   background-color: cyan;
+  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
 `;
 
 export default Header;
