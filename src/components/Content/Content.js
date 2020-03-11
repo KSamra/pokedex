1089c15c11c157fa5e6b1f1bfe51f551267fd53e
Kavan Samra <kssamra@ucsc.edu>
2020-03-10 19:22:07 -0700

    playing around with grid sizing

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
 import React from 'react';
 
+import Card from '../Card/Card';
 
 const StyledContent = styled.main`
   grid-area: content;
   background-color: black;
+  display: grid;
+
+  grid-template-columns: auto auto auto auto;
+
 `;
 
 const Content = (props) => {
   return (
     <StyledContent>
-
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/> 
     </StyledContent>
   )
 };
